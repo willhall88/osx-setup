d7e29830bb48697544b697f04385a0c292a1293d
Sroop <sroopsunar@hotmail.com>
2015-03-27 00:17:45 +0000

    My config.fish

--- fish/config/config.fish
+++ fish/config/config.fish
@@ -1,19 +1,27 @@
-set fish_path $HOME/.osx-setup/fish
+# Path to mac-setup directory
+set fish_path $HOME/.mac-setup/fish
 
-set fish_theme agnoster
+# Theme
+set fish_theme cbjohnson
 
-set fish_plugins theme jump git-flow chruby vi-mode brew tmux
+# Enable plugins by adding their name separated by a space to the line below.
+set fish_plugins theme jump git-flow chruby bundler rbenv sublime rails brew gem localhost
 
-eval sh $HOME/.config/base16-shell/base16-default.dark.sh
-
-if not contains $fish_path/functions/ $fish_function_path
-  set fish_function_path $fish_path/functions/ $fish_function_path
-end
-
+# Set fish_custom to the custom folder path
 if not set -q fish_custom
   set -g fish_custom $fish_path/custom
 end
 
+# Add functions defined in oh-my-fish/functions to the path.
+if not contains $fish_path/functions/ $fish_function_path
+  set fish_function_path $fish_path/functions/ $fish_function_path
+end
+
+# Add imported plugins, completions and themes. Customize imported
+# commands via the $fish_path/custom directory, for example create
+# a directory under $fish_path/custom/themes with the same name as
+# the theme and override any functions/variables there. Rinse and
+# repeat for plugins.
 import plugins/$fish_plugins themes/$fish_theme
 
 # Source all files inside custom directory.
@@ -26,3 +34,11 @@ set fish_function_path $user_function_path $fish_function_path
 
 # Make sure to exit with $status of 1 when reloading the framework.
 or true
+
+# ---------------------------------
+
+
+eval sh $HOME/.config/base16-shell/base16-isotope.dark.sh
+
+set PATH $HOME/.rbenv/bin $PATH
+. (rbenv init -|psub)
