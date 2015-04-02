33033ef3e8d39551302bcd6bc9be755810bab0f6
willhall88 <willhall88@hotmail.com>
2015-04-02 10:10:06 +0100

    still trying to fix

--- fish/config/config.fish
+++ fish/config/config.fish
@@ -1,27 +1,19 @@
-# Path to osx-setup directory
 set fish_path $HOME/.osx-setup/fish
 
-# Theme
-set fish_theme cbjohnson
+set fish_theme agnoster
 
-# Enable plugins by adding their name separated by a space to the line below.
-set fish_plugins theme jump git-flow chruby bundler rbenv sublime rails brew gem localhost
+set fish_plugins theme jump git-flow chruby vi-mode brew tmux
 
-# Set fish_custom to the custom folder path
-if not set -q fish_custom
-  set -g fish_custom $fish_path/custom
-end
+eval sh $HOME/.config/base16-shell/base16-default.dark.sh
 
-# Add functions defined in oh-my-fish/functions to the path.
 if not contains $fish_path/functions/ $fish_function_path
   set fish_function_path $fish_path/functions/ $fish_function_path
 end
 
-# Add imported plugins, completions and themes. Customize imported
-# commands via the $fish_path/custom directory, for example create
-# a directory under $fish_path/custom/themes with the same name as
-# the theme and override any functions/variables there. Rinse and
-# repeat for plugins.
+if not set -q fish_custom
+  set -g fish_custom $fish_path/custom
+end
+
 import plugins/$fish_plugins themes/$fish_theme
 
 # Source all files inside custom directory.
@@ -32,9 +24,5 @@ end
 # Prepend extracted user functions so they have the highest priority.
 set fish_function_path $user_function_path $fish_function_path
 
-eval sh $HOME/.config/base16-shell/base16-isotope.dark.sh
-
 # Make sure to exit with $status of 1 when reloading the framework.
 or true
-
-
--- fish/setup/fish.fish
+++ fish/setup/fish.fish
@@ -1,7 +1,9 @@
 function setup_fish
   _check_dot_config
+  _add_base16
   _check_fish_conf_dir
   _link_fish
+  _install_completions
 end
 
 function _check_dot_config
@@ -24,5 +26,21 @@ function _link_fish
   else
     echo Linking the configuration file
     ln -s "$HOME/.osx-setup/fish/config/config.fish" "$HOME/.config/fish/config.fish"
+  end
+end
+
+function _add_base16
+  if not test -e ~/.config/base16-shell
+    git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+    echo 'eval sh $HOME/.config/base16-shell/base16-default.dark.sh' >> ~/.config/fish/config.fish
+  else
+    echo It seems that you have base 16 already installed, nice!
+  end
+end
+
+function _install_completions
+  if not test -e ~/.config/fish/completions
+    mkdir ~/.config/fish/completions
+    curl -LSso ~/.config/fish/completions/docker.fish https://raw.github.com/barnybug/docker-fish-completion/master/docker.fish
   end
 end
